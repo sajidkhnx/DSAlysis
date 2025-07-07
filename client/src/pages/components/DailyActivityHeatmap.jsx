82920d122aab5f859e4c3e8f110f59bd698308b6
Mohd Sajid Khan <khansahab9757@gmail.com>
2025-07-07 14:09:02 +0530

    Update DailyActivityHeatmap.jsx

--- client/src/pages/components/DailyActivityHeatmap.jsx
+++ client/src/pages/components/DailyActivityHeatmap.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CalendarHeatmap from "react-calendar-heatmap";
 // import "react-calendar-heatmap/dist/styles.css";
-import '../../styles/heatmap.css';
+import '../css/heatmap.css'; // Custom styles for heatmap
 import { useQuestions } from "../../QuestionsContext";
 import { endOfToday, subDays, format, getMonth, getYear } from "date-fns";
 
